48ded88db0ada9be6d0ab430f31bd073b2090e56
Anh Luu <luutuananh@hotmail.com>
2025-08-05 12:44:23 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = new Hono();
 // Important! No trailing slash
 const allowedOrigins = new Set([
   "http://localhost:5173",
-  "https://blazenote-frontend-705.pages.dev/",
+  "https://blazenote-frontend-705.pages.dev",
 ]);
 
 // https://hono.dev/docs/middleware/builtin/cors
